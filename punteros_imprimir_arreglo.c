a7a42fe7b5e8bd251ce3ccfb087935e208091fc6
SantiaGo120602 <92653737+SantiaGo120602@users.noreply.github.com>
2021-10-21 23:32:07 -0500

    Create  punteros_imprimir_arreglo.c

---
+++ punteros_imprimir_arreglo.c
@@ -0,0 +1,14 @@
+//Programa que ingresa e imprime elementos de un arreglo mediante punteros.
+//autor: Santiago Javier Vivas Piamba
+//versión 1.0
+
+#include <stdio.h>
+
+
+int main()
+{
+	
+	char arreglo1[30];
+	char arreglo2[30];
+
+}
